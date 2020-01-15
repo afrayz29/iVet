aafe96d6e27022a7264affc351dd35bdf6afe361
Ivette Dominguez <ivettedominguez104@gmail.com>
2020-01-15 16:12:21 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -15,3 +15,42 @@ db = SQLAlchemy()
 #             "username": self.username,
 #             "email": self.email
 #         }
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
+    date = db.Column(db.Integer(120), unique=False, nullable=False)
+    petname = db.relationship('DogUser', backref='user', lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.firstName
+
+    def serialize(self):
+        return {
+            "firstName": self.firstName,
+            "email": self.email,
+            "password": self.password,
+            "date": self.date
+    }
+class DogUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(80), unique=False, nullable=False)
+    age = db.Column(db.Integer(120), unique=False, nullable=False)
+    breed_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+    img = db.Column(db.String(120), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+     
+    def __repr__(self):
+        return '<DogUser %r>' % self.Name
+
+    def serialize(self):
+        return {
+            "Name": self.Name,
+            "age": self.age,
+            "breed": self.breed,
+            "img": self.img
+    }
